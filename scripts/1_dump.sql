316eb1c5a27bc1816cf26dfe491fa377dbeadd73
Max Martini <maxmmartini@gmail.com>
2024-04-27 20:40:14 -0300

    Primeiro commit

---
+++ scripts/1_dump.sql
@@ -0,0 +1,111 @@
+-- MariaDB dump 10.18  Distrib 10.4.17-MariaDB, for Win64 (AMD64)
+--
+-- Host: 127.0.0.1    Database: main
+-- ------------------------------------------------------
+-- Server version	10.3.38-MariaDB-1:10.3.38+maria~ubu2004
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cliente` (
+                           `id` int(11) NOT NULL AUTO_INCREMENT,
+                           `cliente` varchar(100) NOT NULL,
+                           `cnpj` char(14) NOT NULL,
+                           `status` enum('ativo','inativo') NOT NULL,
+                           PRIMARY KEY (`id`),
+                           UNIQUE KEY `cnpj` (`cnpj`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (15,'Mondelez','0001','ativo'),(16,'Troca','0002','ativo'),(17,'Swift','0003','ativo'),(18,'Lactalis','0004','ativo'),(19,'LP','0005','ativo'),(20,'Mobile','0006','ativo'),(21,'ACL','0007','ativo'),(22,'Bateria moura','0008','ativo'),(23,'Cobrascam','0009','ativo'),(24,'Intalog','0010','ativo'),(25,'Klog','0011','ativo'),(26,'Levcarca','0012','ativo'),(27,'Log20','0013','ativo'),(28,'Open','0014','ativo');
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `solicitacao`
+--
+
+DROP TABLE IF EXISTS `solicitacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `solicitacao` (
+                               `id` int(11) NOT NULL AUTO_INCREMENT,
+                               `data` datetime NOT NULL,
+                               `cliente` varchar(255) NOT NULL,
+                               `placa` varchar(20) NOT NULL,
+                               PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `solicitacao`
+--
+
+LOCK TABLES `solicitacao` WRITE;
+/*!40000 ALTER TABLE `solicitacao` DISABLE KEYS */;
+INSERT INTO `solicitacao` VALUES (1,'2024-04-17 12:20:00','Mondelez','KQV-4334'),(2,'2024-04-17 13:30:00','Troca','RJU-8814'),(3,'2024-04-25 18:56:57','Camila','LLE2319'),(4,'2024-04-25 23:55:52','mondelez','CAM2004'),(5,'2024-04-26 00:28:24','mondelez','CAM2004');
+/*!40000 ALTER TABLE `solicitacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+                           `id` int(11) NOT NULL AUTO_INCREMENT,
+                           `nome` varchar(100) NOT NULL,
+                           `email` varchar(100) NOT NULL,
+                           `cpf` char(11) NOT NULL,
+                           `papel` enum('administrador','operador','financeiro','comercial','gerente') NOT NULL,
+                           `senha` varchar(255) NOT NULL,
+                           PRIMARY KEY (`id`),
+                           UNIQUE KEY `email` (`email`),
+                           UNIQUE KEY `cpf` (`cpf`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'Camila','gmlins06@gmail.com','13181333786','administrador','123456'),(2,'Max','maxmmartini@gmail.com','2','administrador','123456');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-04-27 20:36:34
+
