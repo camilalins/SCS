c94dab9e29cafa8f1eac08682efb461ebfd3ecc0
Max Martini <maxmmartini@gmail.com>
2024-05-02 14:20:10 -0300

    Adiciona componente de envio de e-mail por composer[4]

--- html/login/recuperar-senha.php
+++ html/login/recuperar-senha.php
@@ -7,15 +7,15 @@ use PHPMailer\PHPMailer\Exception;
 include_once "../core/BaseController.php";
 require_once "../repo/UsuarioRepositorio.php";
 
-class RecuperarSenhaController extends BaseController
-{
-    public function get()
-    {
+class RecuperarSenhaController extends \controllers\core\BaseController {
+
+    public function get() {
+
         view("/login/recuperar-senha.php");
     }
 
-    public function post()
-    {
+    public function post() {
+
         try {
             // OBTER DADOS
             $email = $_POST["email"];
@@ -30,10 +30,11 @@ class RecuperarSenhaController extends BaseController
                 throw new Exception("E-mail encontrado");
 
             //TODO: ENVIAR E-MAIL DE RECUPREÇÃO DE SENHA
-            if(!send_mail($email, "Recuperação de senha", "<p>Sua nova senha é: XXXXXXXX</p>"))
+            if(!send_mail($email, "Recuperação de senha", "<p>Clique <a href='#'>aqui</a> para recuperar sua senha.</p>"))
                 throw new Exception(send_mail_error());
 
-        } catch (Exception $e) {
+        }
+        catch (Exception $e) {
             view("/login/recuperar-senha.php", [ "erro" => $e->getMessage() ]);
         }
     }
