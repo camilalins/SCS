9b482285add1c49e4b8e4df516ae01f6d78acc3d
Max Martini <maxmmartini@gmail.com>
2024-05-18 14:24:33 -0300

    Ajustes na estrutura de Docker compose, e variavéis de ambiente

--- html/core/Application.php
+++ html/core/Application.php
@@ -4,108 +4,112 @@ namespace core;
 
 class Application {
 
-    public static function run(){
+    public static function run(): void{
 
-        # BASIC PARAMS
-        $uri = $_SERVER["REQUEST_URI"];
-        $method = $_SERVER["REQUEST_METHOD"];
-
-        #INCLUDE CONFIG
-        foreach (glob("config/*.php") as $filename) include_once $filename;
-        foreach (glob("config/*") as $directory) foreach (glob("$directory/*.php") as $filename) include_once $filename;
-
-        #INCLUDE HELPERS
-        foreach (glob("helpers/*.php") as $filename) include_once $filename;
-        foreach (glob("helpers/*") as $directory) foreach (glob("$directory/*.php") as $filename) include_once $filename;
-
-        #INCLUDE CORE
-        foreach (glob("core/*.php") as $filename) include_once $filename;
-        foreach (glob("core/*") as $directory) foreach (glob("$directory/*.php") as $filename) include_once $filename;
-        foreach (glob("core/controllers/*") as $directory) foreach (glob("$directory/*.php") as $filename) include_once $filename;
-
-        #INCLUDE MODELS
-        foreach (glob("models/*.php") as $filename) include_once $filename;
-        foreach (glob("models/*") as $directory) foreach (glob("$directory/*.php") as $filename) include_once $filename;
-
-        #INCLUDE REPO
-        foreach (glob("repo/*.php") as $filename) include_once $filename;
-        foreach (glob("repo/*") as $directory) foreach (glob("$directory/*.php") as $filename) include_once $filename;
-
-        #INCLUDE CONTROLLERS
-        foreach (glob("controllers/*.php") as $filename) include_once $filename;
-        foreach (glob("controllers/*") as $directory) foreach (glob("$directory/*.php") as $filename) include_once $filename;
-        foreach (glob("controllers/api/*") as $directory) foreach (glob("$directory/*.php") as $filename) include_once $filename;
-
-        #SESSION
-        session_start(['cookie_lifetime' => 0, 'cookie_secure' => true, 'cookie_httponly' => true]);
-
-        $cookie = session_get_cookie_params();
-        $sessid = session_id();
-        setcookie(
-            'PHPSESSID', //name
-            $sessid, //value
-            time() + (SESSION_TIMEOUT?:60*30), //expires in seconds
-            $cookie['path'],//path
-            $cookie['domain'],//domain
-            true, //secure
-            true  //httpOnly
-        );
-
-        #WARNING LEVEL
-        error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
-
-        #DEBUG LOG
-        if (DEBUG_MODE == 1) {
-            openlog("localhost", LOG_PID | LOG_PERROR, LOG_LOCAL0);
-            syslog(LOG_INFO, $uri);
-        }
-
-        #SEEKING ROUTES
-        foreach (glob("controllers/*.php") as $filename) {
-            $namespace = "\\controllers\\";
-            Application::seekRoutes($namespace, $filename, $routes);
-        }
-        foreach (glob("controllers/*") as $directory) {
-            $namespace = "\\".str_replace("/", "\\", $directory)."\\";
-            foreach (glob("$directory/*.php") as $filename)
-                Application::seekRoutes($namespace, $filename, $routes);
-        }
-        foreach (glob("controllers/api/*") as $directory) {
-            $namespace = "\\".str_replace("/", "\\", $directory)."\\";
-            foreach (glob("$directory/*.php") as $filename)
-                Application::seekRoutes($namespace, $filename, $routes);
-        }
-
-        #SEEKING ROUTES - DEFINE CONTROLLER & ACTION
-
-        $controller = Application::defineController($uri, $routes);
-        $action = strtolower($method); //$action = $path[ACTN] ?: "index";
-
-        #SEEKING ROUTES - DEFINE PATH VARIBELS
-        if($routes[$controller]) {
-            $parthVars = str_replace($routes[$controller], "", $uri);
-            $parthVars = str_starts_with($parthVars, "/") ? substr($parthVars, 1, strlen($parthVars)) : $parthVars;
-            $GLOBALS["path"] = explode("/", $parthVars);
-        }
-
-        #./SEEKING ROUTES
-
-        # MOCKUP OPTION
-        if(MOCKUP_MODE == 1) redirect(MOCKUP_ROOT);
-
-        #ROUTE CALL
         try {
 
-            $ctrl = new $controller();
-            $ctrl->$action();
+            # BASIC PARAMS
+            $uri = $_SERVER["REQUEST_URI"];
+            $method = $_SERVER["REQUEST_METHOD"];
+            $status = http_response_code();
+
+            #INCLUDE CONFIG
+            foreach (glob("config/*.php") as $filename) include_once $filename;
+            foreach (glob("config/*") as $directory) foreach (glob("$directory/*.php") as $filename) include_once $filename;
+
+            #INCLUDE HELPERS
+            foreach (glob("helpers/*.php") as $filename) include_once $filename;
+            foreach (glob("helpers/*") as $directory) foreach (glob("$directory/*.php") as $filename) include_once $filename;
+
+            #INCLUDE CORE
+            foreach (glob("core/*.php") as $filename) include_once $filename;
+            foreach (glob("core/*") as $directory) foreach (glob("$directory/*.php") as $filename) include_once $filename;
+            foreach (glob("core/controllers/*") as $directory) foreach (glob("$directory/*.php") as $filename) include_once $filename;
+
+            #INCLUDE MODELS
+            foreach (glob("models/*.php") as $filename) include_once $filename;
+            foreach (glob("models/*") as $directory) foreach (glob("$directory/*.php") as $filename) include_once $filename;
+
+            #INCLUDE REPO
+            foreach (glob("repo/*.php") as $filename) include_once $filename;
+            foreach (glob("repo/*") as $directory) foreach (glob("$directory/*.php") as $filename) include_once $filename;
+
+            #INCLUDE CONTROLLERS
+            foreach (glob("controllers/*.php") as $filename) include_once $filename;
+            foreach (glob("controllers/*") as $directory) foreach (glob("$directory/*.php") as $filename) include_once $filename;
+            foreach (glob("controllers/api/*") as $directory) foreach (glob("$directory/*.php") as $filename) include_once $filename;
+
+            #SESSION
+            session_start(['cookie_lifetime' => 0, 'cookie_secure' => true, 'cookie_httponly' => true]);
+
+            $cookie = session_get_cookie_params();
+            $sessid = session_id();
+            setcookie(
+                'PHPSESSID', //name
+                $sessid, //value
+                time() + (SESSION_TIMEOUT?:60*30), //expires in seconds
+                $cookie['path'],//path
+                $cookie['domain'],//domain
+                true, //secure
+                true  //httpOnly
+            );
+
+            #WARNING LEVEL
+            error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
+
+            #DEBUG LOG
+            if (DEBUG_MODE == 1) {
+                openlog(getHostByName(getHostName()), LOG_PID | LOG_PERROR, LOG_LOCAL0);
+                syslog(LOG_INFO, now(DEF_LANG, "d/M/Y:H:i:s")."+0000 \"$method $uri\" $status");
+            }
+
+            #SEEKING ROUTES
+            $routes = [];
+            foreach (glob("controllers/*.php") as $filename) {
+                $namespace = "\\controllers\\";
+                Application::seekRoutes($namespace, $filename, $routes);
+            }
+            foreach (glob("controllers/*") as $directory) {
+                $namespace = "\\".str_replace("/", "\\", $directory)."\\";
+                foreach (glob("$directory/*.php") as $filename)
+                    Application::seekRoutes($namespace, $filename, $routes);
+            }
+            foreach (glob("controllers/api/*") as $directory) {
+                $namespace = "\\".str_replace("/", "\\", $directory)."\\";
+                foreach (glob("$directory/*.php") as $filename)
+                    Application::seekRoutes($namespace, $filename, $routes);
+            }
+
+            #SEEKING ROUTES - DEFINE CONTROLLER & ACTION
+
+            $controller = Application::defineController($uri, $routes);
+            $action = strtolower($method);
+
+            #SEEKING ROUTES - DEFINE PATH VARIBELS
+            if($routes[$controller]) {
+                $parthVars = str_replace($routes[$controller], "", $uri);
+                $parthVars = str_starts_with($parthVars, "/") ? substr($parthVars, 1, strlen($parthVars)) : $parthVars;
+                $GLOBALS["path"] = explode("/", $parthVars);
+            }
+
+            #./SEEKING ROUTES
+
+            # MOCKUP OPTION
+            if(MOCKUP_MODE == 1) redirect(MOCKUP_ROOT);
+
+            #ROUTE CALL
+            self::routeCall($controller, $action);
+
         }
-        catch (\Error $e) {
-            print("Erro na rota". (DEBUG_MODE == 1 && DEBUG_LEVEL == DEBUG_LEVEL_HIGH ? ": {$e->getMessage()}. arquivo: <b>{$e->getFile()} {$e->getLine()}</b>" : ""));
+        catch (\Exception $e) {
+            print("Falha na aplicação: ". (DEBUG_MODE == 1 && DEBUG_LEVEL == DEBUG_LEVEL_HIGH ? ": {$e->getMessage()}. arquivo: <b>{$e->getFile()} {$e->getLine()}</b>" : ""));
         }
+
         exit();
     }
 
-    private static function seekRoutes($namespace, $filename, &$routes){
+    private static function seekRoutes($namespace, $filename, &$routes): void
+    {
         $controller = $namespace.pathinfo($filename, PATHINFO_FILENAME);
         $classInfo = new \ReflectionClass($controller);
         $classComments = array_filter(array_map(function ($part) { return trim($part," *"); }, explode("\n", $classInfo->getDocComment())), function ($part) { return $part != "/"; });
@@ -120,7 +124,8 @@ class Application {
 
     }
 
-    private static function seekActions($namespace, $filename, $action, &$actions){
+    private static function seekActions($namespace, $filename, $action, &$actions): void
+    {
         $controller = $namespace.pathinfo($filename, PATHINFO_FILENAME);
         $methodInfo = new \ReflectionMethod($controller, $action);
         $methodComments = array_filter(array_map(function ($part) { return trim($part," *"); }, explode("\n", $methodInfo->getDocComment() )), function ($part) { return $part != "/"; });
@@ -129,7 +134,7 @@ class Application {
         //TODO: CONFIGURAR LEITURA DE REQUEST METHOD POR COMENTARIO
     }
 
-    private static function defineController($uri, $routes){
+    private static function defineController($uri, $routes=[]){
 
         foreach ($routes as $key => $value) {
 
@@ -137,6 +142,18 @@ class Application {
             if($uri == "/") preg_match("/^$path/", $uri, $matches);
             else preg_match("/^$path$/", $uri, $matches);
             if ($matches) return $key;
+        }
+    }
+
+    private static function routeCall($controller, $action): void{
+
+        try {
+
+            $ctrl = new $controller();
+            $ctrl->$action();
+        }
+        catch (\Error $e) {
+            print("Erro na rota". (DEBUG_MODE == 1 && DEBUG_LEVEL == DEBUG_LEVEL_HIGH ? ": {$e->getMessage()}. arquivo: <b>{$e->getFile()} {$e->getLine()}</b>" : ""));
         }
     }
 
--- html/helpers/date-time.php
+++ html/helpers/date-time.php
@@ -1,5 +1,14 @@
 <?php
+/**
+ * @param $locale
+ * @param $format
+ * @return string
+ * @throws Exception
+ */
+function now($locale=null, $format=null): string {
 
-function now() {
-    return date("Y-m-d H:i:s");
+    return match ($locale) {
+        "pt-BR" => (new DateTime("now", new DateTimeZone("America/Sao_Paulo")))->format($format ?: "d/m/Y H:i:s"),
+        default => date("Y-m-d H:i:s"),
+    };
 }
