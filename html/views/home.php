c5a78c38356268d124f773cebecf3fd476fe59a6
srtabbg <gmlins06@gmail.com>
2024-05-19 21:29:31 -0300

    Inserido o comando <!-- Font Awesome -->     <script src="https://kit.fontawesome.com/eaf468bfde.js" crossorigin="anonymous"></script> Para sinalizar corretamente os ícones do sistema
    Ajuste no botão cadastrar que mostra o modal na view cadastro cliente
    
    Funcionalidade do modal ajustada

--- html/views/home.php
+++ html/views/home.php
@@ -6,48 +6,39 @@
         <div class="container">
             <div class="row">
                 <div class="nav">
-                    <a class="navbar-brand " href="<?=HOME_PAGE?>">
-                        <i class="fa fa-angle-left" aria-hidden="true"></i>
-                    </a>
                     <h3 class="text-left display-5">Olá <span class="unique-text"><?=user()->getNome()?></span>!</h3>
                 </div>
                 <div class="row gy-4 gx-4 ">
                     <!-- service number 1 -->
-                    <a href="/clientes" class="col-md-6 col-12 col-lg-4 mx-auto" data-aos="fade-down" data-aos-delay="200" >
+                    <a href="/clientes" class="p-3 col-md-6 col-12 col-lg-4 mx-auto" data-aos="fade-down" data-aos-delay="200" >
                         <div>
                             <div class="box box-service box-hover aos-init aos-animate" data-aos="fade-right" data-aos-delay="250">
-                                <div class="box-icon my-2">
-                                    <i class="fa-solid fa-truck-front"></i>
-                                </div>
+
                                 <h2 class="title-2 my-2 ">Clientes</h2>
                                 <p>Mantenha os clientes atualizados.</p>
-                                <b class="my-2 learn-more">cadastrar Cliente <i class="fa-solid fa-angle-right"></i></b>
+                                <b class="my-2 learn-more">cadastrar Cliente <i class="fa-solid fa-angle-right fs-5 px-2"></i></b>
                             </div>
                         </div>
                     </a>
                     <!-- service number 2 -->
-                    <a href="/relatorios" class="col-md-6 col-12 col-lg-4 mx-auto" data-aos="fade-down" data-aos-delay="200" >
+                    <a href="/relatorios" class="p-3 col-md-6 col-12 col-lg-4 mx-auto" data-aos="fade-down" data-aos-delay="200" >
                         <div>
                             <div class="box box-service box-hover aos-init aos-animate" data-aos="fade-right" data-aos-delay="250">
-                                <div class="box-icon my-2">
-                                    <i class="fa-solid fa-clipboard"></i>
-                                </div>
+
                                 <h2 class="title-2 my-2 ">Relatórios</h2>
                                 <p>Acompanhe suas KPI's.</p>
-                                <b class="my-2 learn-more">Acessar <i class="fa-solid fa-angle-right"></i></b>
+                                <b class="my-2 learn-more">Acessar <i class="fa-solid fa-angle-right fs-5 px-2"></i></b>
                             </div>
                         </div>
                     </a>
                     <!-- service number 3 -->
-                    <a href="/solicitacoes" class="col-md-6 col-12 col-lg-4 mx-auto" data-aos="fade-down" data-aos-delay="200" >
+                    <a href="/solicitacoes" class="p-3  col-md-6 col-12 col-lg-4 mx-auto" data-aos="fade-down" data-aos-delay="200" >
                         <div>
                             <div class="box box-service box-hover aos-init aos-animate" data-aos="fade-right" data-aos-delay="250">
-                                <div class="box-icon my-2">
-                                    <i class="fa-solid fa-motorcycle"></i>
-                                </div>
+
                                 <h2 class="title-2 my-2 ">Solicitações</h2>
                                 <p>Cadastre todas as solicitações.</p>
-                                <b class="my-2 learn-more">cadastrar Solicitação<i class="fa-solid fa-angle-right"></i></b>
+                                <b class="my-2 learn-more">cadastrar Solicitação<i class="fa-solid fa-angle-right fs-5 px-2"></i></b>
                             </div>
                         </div>
                     </a>
